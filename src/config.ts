06a98d9ce5c4fca5d6cf2fe98579d6c2647f8947
Arturo <arturo@interpegasus.com>
2020-05-03 12:33:25 -0500

    Rendertron

--- src/config.ts
+++ src/config.ts
@@ -17,41 +17,41 @@
  * the License.
  */
 
-"use strict";
+'use strict';
 
-import * as fse from "fs-extra";
-import * as path from "path";
+import * as fse from 'fs-extra';
+import * as path from 'path';
 
-const CONFIG_PATH = path.resolve(__dirname, "../config.json");
+const CONFIG_PATH = path.resolve(__dirname, '../config.json');
+
 
 export type Config = {
-  cache: "datastore" | "memory" | null;
-  timeout: number;
-  port: string;
-  host: string;
-  width: number;
-  height: number;
-  headers: { [key: string]: string };
-  whiteList: string[];
+    cache: 'datastore' | 'memory' | null;
+    timeout: number;
+    port: string;
+    host: string
+    width: number;
+    height: number;
+    headers: { [key: string]: string };
 };
 
 export class ConfigManager {
-  public static config: Config = {
-    cache: null,
-    timeout: 10000,
-    port: "3000",
-    host: "0.0.0.0",
-    width: 1000,
-    height: 1000,
-    headers: {},
-    whiteList: [],
-  };
+    public static config: Config = {
+        cache: null,
+        timeout: 10000,
+        port: '3000',
+        host: '0.0.0.0',
+        width: 1000,
+        height: 1000,
+        headers: {}
+    };
 
-  static async getConfiguration(): Promise<Config> {
-    // Load config.json if it exists.
-    if (fse.pathExistsSync(CONFIG_PATH)) {
-      ConfigManager.config = Object.assign(ConfigManager.config, await fse.readJson(CONFIG_PATH));
+    static async getConfiguration(): Promise<Config> {
+        // Load config.json if it exists.
+        if (fse.pathExistsSync(CONFIG_PATH)) {
+            ConfigManager.config = Object.assign(ConfigManager.config, await fse.readJson(CONFIG_PATH));
+        }
+        return ConfigManager.config;
     }
-    return ConfigManager.config;
-  }
 }
+
